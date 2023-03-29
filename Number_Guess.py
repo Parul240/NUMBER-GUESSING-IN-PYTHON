c56217a560df348b217c1fd819e9a99804b27223
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-29 17:27:26 +0530

    Create Number_Guess.py

---
+++ Number_Guess.py
@@ -0,0 +1,40 @@
+#Number guessing game in python
+import random
+import time
+
+print("""Hey! I'm Parul Sharma here, I hope you are doing great.
+WELCOME to the NUMBER GUESSING GAME.
+I am going to pick a number between 1 and 100.
+You have to guess that number within 10 attempts.
+GOOD LUCK! """)
+
+time.sleep(2)
+print("Picking a number....")
+
+#selecting random number
+num=random.randint(1,100)
+
+time.sleep(2)
+#taking input for guess number
+guess=int(input("What's your guess?: "))
+
+count=0
+
+#applying wrong guess condition
+while guess!=num:
+    count+=1
+    if count==10:
+        print("""You have finished your attempt limit.
+Better Luck Next Time!""")
+        break
+    elif guess<num:
+        guess=int(input("Guess the higher number: "))
+    elif guess>num:
+        guess=int(input("Guess the lower number: "))
+
+
+#for right guess
+if guess==num:
+    print(f"CONGRATS! you guess the correct number {num}.you took {count + 1} times to guess the number")
+
+
